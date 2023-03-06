42609617bab17648d2d68822fe7952047310bb49
jxl55 <33408518+jxl55@users.noreply.github.com>
2023-03-05 17:33:26 -0800

    code spine from hw 5

---
+++ hw5_localize.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+#
+#   hw5_localize.py
+#
+#   Homework 5 code framework to localize a robot in a grid...
+#
+#   Places to edit are marked as TODO.
+#
+import numpy as np
+
+from hw5_utilities import Visualization, Robot
+
+
+#
+#  Define the Walls
+#
+w = ['xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+     'x               x               x               x',
+     'x                x             x                x',
+     'x                 x           x                 x',
+     'x        xxxx      x         x                  x',
+     'x        x   x      x       x                   x',
+     'x        x    x      x     x      xxxxx         x',
+     'x        x     x      x   x     xxx   xxx       x',
+     'x        x      x      x x     xx       xx      x',
+     'x        x       x      x      x         x      x',
+     'x        x        x           xx         xx     x',
+     'x        x        x           x           x     x',
+     'x        x        x           x           x     x',
+     'x        x        x           x           x     x',
+     'x                 xx         xx           x     x',
+     'x                  x         x                  x',
+     'x                  xx       xx                  x',
+     'x                   xxx   xxx                   x',
+     'x                     xxxxx         x           x',
+     'x                                   x          xx',
+     'x                                   x         xxx',
+     'x            x                      x        xxxx',
+     'x           xxx                     x       xxxxx',
+     'x          xxxxx                    x      xxxxxx',
+     'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx']
+
+walls = np.array([[1.0*(c == 'x') for c in s] for s in w])
+rows  = np.size(walls, axis=0)
+cols  = np.size(walls, axis=1)
+
+
+#
+#  Prediction
+#
+#    bel         Grid of probabilities (current belief)
+#    drow, dcol  Delta in row/col
+#    probCmd     Modeled probability of command executing
+#    prd         Grid of probabilities (prediction)
+#
+def computePrediction(bel, drow, dcol, probCmd):
+    # Prepare an empty prediction grid.
+    prd = np.zeros((rows,cols))
+
+    # Iterate over/determine the probability for all (non-wall) elements.
+    for row in range(1, rows-1):
+        for col in range(1, cols-1):
+            if (drow != 0 or dcol != 0) and walls[row - drow, col - dcol] == 0:
+                prd[row, col] = bel[row - drow, col - dcol] * probCmd + bel[row, col] * (1-probCmd)
+                if walls[row,col] == 1:
+                    prd[row,col] = 0
+                    prd[row - drow, col - dcol] = bel[row - drow, col - dcol] * probCmd
+            # TODO.... if ... prd[row, col] ...
+
+    # Return the prediction grid
+    return prd
+
+
+#
+#  Measurement Update (Correction)
+#
+#    prior       Grid of prior probabilities (belief)
+#    probSensor  Grid of probability that (sensor==True)
+#    sensor      Value of sensor
+#    post        Grid of posterior probabilities (updated belief)
+#
+def updateBelief(prior, probSensor, sensor):
+    # Update the belief based on the sensor reading, which can be one
+    # of two cases: (sensor==True) or (sensor==False)
+    post = np.zeros((rows,cols))
+    if (sensor):
+        post = probSensor * prior
+        # post = TODO
+    else:
+        post = (1 - probSensor) * prior
+        # post = TODO
+
+    # Normalize.
+    s = np.sum(post)
+    if (s == 0.0):
+        print("LOST ALL BELIEF.  SHOULD NOT HAPPEN.  STARTING OVER!!!!")
+        post = 1.0 - walls
+        s    = np.sum(post)
+
+    post = (1.0/s) * post
+    return post
+
+
+#
+#  Pre-compute the Sensor Probability Grid
+#
+#    drow, dcol    Direction in row/col
+#    probProximal  List of probability that sensor triggers at dist=(index+1)
+#    prob          Grid of probability that (sensor==True)
+#
+def precomputeSensorProbability(drow, dcol, probProximal):
+    # Prepare an empty probability grid.
+    prob = np.zeros((rows, cols))
+
+    # Pre-compute the probability for each grid element, knowing the
+    # walls and the direction of the sensor.
+    for row in range(1, rows-1):
+        for col in range(1, cols-1):
+            # if (drow != 0 or dcol != 0) and walls[row + drow, col + dcol] == 1:
+            #     prob[row, col] = probProximal[0]
+            for dist in range(1, len(probProximal)+1):
+                if (drow != 0 or dcol != 0) and walls[row + dist*drow, col + dist*dcol] == 1:
+                    prob[row, col] = probProximal[dist-1]
+                    break
+            # TODO ....  prob[row, col] = ....
+
+    # Return the computed grid.
+    return prob
+
+
+# 
+#
+#  Main Code
+#
+def main():
+    # Initialize the figure.
+    visual = Visualization(walls)
+
+    # TODO... PICK WHAT THE "REALITY" SHOULD SIMULATE:
+    # Initialize the robot simulation.
+    # part (a)
+    # robot  = Robot(walls)
+    # Part (b)
+    robot = Robot(walls, row=12, col=26)
+    # Part (c)
+    # robot = Robot(walls, row=12, col=26, probProximal = [0.9, 0.6, 0.3])
+    # Part (d), (e)
+    # robot = Robot(walls, row=15, col=47,
+    #               probCmd = 0.8, probProximal = [0.9, 0.6, 0.3])
+    # And to play:
+    # robot = Robot(walls, probCmd = 0.8, probProximal = [0.9, 0.6, 0.3])
+
+
+    # TODO... PICK WHAT YOUR LOCALIZATION SHOULD ASSUME:
+    # Pick the algorithm assumptions:
+    probCmd      = 1.0                  # Part (a/b), (c), (d)
+    # probCmd      = 0.8                  # Part (e), or to play
+    probProximal = [1.0]                # Part (a/b)
+    # probProximal = [0.9, 0.6, 0.3]      # Part (c), (d), (e), or to play
+
+    # Report.
+    print("Localization is assuming probCmd = " + str(probCmd) +
+          " and probProximal = " + str(probProximal))
+
+
+    # Pre-compute the probability grids for each sensor reading.
+    probUp    = precomputeSensorProbability(-1,  0, probProximal)
+    probRight = precomputeSensorProbability( 0,  1, probProximal)
+    probDown  = precomputeSensorProbability( 1,  0, probProximal)
+    probLeft  = precomputeSensorProbability( 0, -1, probProximal)
+
+    # Show the sensor probability maps.
+    # visual.Show(probUp)
+    # input("Probability of proximal sensor up reporting True")
+    # visual.Show(probRight)
+    # input("Probability of proximal sensor right reporting True")
+    # visual.Show(probDown)
+    # input("Probability of proximal sensor down reporting True")
+    # visual.Show(probLeft)
+    # input("Probability of proximal sensor left reporting True")
+
+
+    # Start with a uniform belief grid.
+    bel = 1.0 - walls
+    bel = (1.0/np.sum(bel)) * bel
+
+
+    # Loop continually.
+    while True:
+        # Show the current belief.  Also show the actual position.
+        visual.Show(bel, robot.Position())
+
+        # Get the command key to determine the direction.
+        while True:
+            key = input("Cmd (q=quit, i=up, m=down, j=left, k=right) ?")
+            if   (key == 'q'):  return
+            elif (key == 'i'):  (drow, dcol) = (-1,  0) ; break
+            elif (key == 'm'):  (drow, dcol) = ( 1,  0) ; break
+            elif (key == 'j'):  (drow, dcol) = ( 0, -1) ; break
+            elif (key == 'k'):  (drow, dcol) = ( 0,  1) ; break
+
+        # Move the robot in the simulation.
+        robot.Command(drow, dcol)
+
+
+        # Compute a prediction.
+        prd = computePrediction(bel, drow, dcol, probCmd)
+        #visual.Show(prd)
+        #input("Showing the prediction")
+
+        # Check the prediction.
+        if abs(np.sum(prd) - 1.0) > 1e-12:
+            print("WARNING: Prediction does not add up to 100%")
+
+
+        # Correct the prediction/execute the measurement update.
+        bel = prd
+        bel = updateBelief(bel, probUp,    robot.Sensor(-1,  0))
+        bel = updateBelief(bel, probRight, robot.Sensor( 0,  1))
+        bel = updateBelief(bel, probDown,  robot.Sensor( 1,  0))
+        bel = updateBelief(bel, probLeft,  robot.Sensor( 0, -1))
+
+
+if __name__== "__main__":
+    main()
---
+++ hw5_utilities.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+#
+#   hw5_utilities.py
+#
+#   Two classes to help the visualization and simulation.
+#
+#
+#   VISUALIZATION:
+#
+#     visual = Visualization(walls)
+#     visual.Show(prob)
+#     visual.Show(prob, (row, col))
+#
+#     walls         NumPy 2D array defining both the grid size
+#                   (rows/cols) and walls (being non-zero elements).
+#     prob          NumPy 2D array of probabilities, values 0 to 1.
+#
+#   visual.Show() will visualize the probabilities.  The second form
+#   will also mark the (row,col) position with an 'x'.
+#
+#
+#   ROBOT SIMULATION:
+#
+#     robot = Robot(walls, row=0, col=0, probCmd=1.0, probProximal=[1.0])
+#     robot.Command(drow, dcol)
+#     True/False = robot.Sensor(drow, dcol)
+#     (row, col) = robot.Position()
+#
+#     probCmd       Probability the command is executed (0 to 1).
+#     probProximal  List of probabilities (0 to 1).  Each element is
+#                   the probability that the proximity sensor will
+#                   fire at a distance of (index+1 = 1,2,3,etc)
+#     (drow, dcol)  Delta up/right/down/left: (-1,0) (0,1) (1,0) (0,-1)
+#
+#   Simulate a robot, to give us the sensor readings.  If the starting
+#   row/col are not given, pick them randomly.  Note both the command
+#   and the sensor may be configured to a random probability level.
+#   The robot.Position() is intended for debugging and visualization
+#   only, as it gives the actual position.
+#
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+#
+#   Probailiity Grid Visualization
+#
+class Visualization():
+    def __init__(self, walls):
+        # Save the walls and determine the rows/cols:
+        self.walls = walls
+        self.spots = np.sum(np.logical_not(walls))
+        self.rows  = np.size(walls, axis=0)
+        self.cols  = np.size(walls, axis=1)
+
+        # Clear the current, or create a new figure.
+        plt.clf()
+
+        # Create a new axes, enable the grid, and set axis limits.
+        plt.axes()
+        plt.grid(False)
+        plt.gca().axis('off')
+        plt.gca().set_aspect('equal')
+        plt.gca().set_xlim(0, self.cols)
+        plt.gca().set_ylim(self.rows, 0)
+
+        # Add the row/col numbers.
+        for row in range(0, self.rows, 2):
+            plt.gca().text(         -0.3, 0.5+row, '%d'%row,
+                           verticalalignment='center',
+                           horizontalalignment='right')
+        for row in range(1, self.rows, 2):
+            plt.gca().text(self.cols+0.3, 0.5+row, '%d'%row,
+                           verticalalignment='center',
+                           horizontalalignment='left')
+        for col in range(0, self.cols, 2):
+            plt.gca().text(0.5+col,          -0.3, '%d'%col,
+                           verticalalignment='bottom',
+                           horizontalalignment='center')
+        for col in range(1, self.cols, 2):
+            plt.gca().text(0.5+col, self.rows+0.3, '%d'%col,
+                           verticalalignment='top',
+                           horizontalalignment='center')
+
+        # Draw the grid, zorder 1 means draw after zorder 0 elements.
+        for row in range(self.rows+1):
+            plt.gca().axhline(row, lw=1, color='k', zorder=1)
+        for col in range(self.cols+1):
+            plt.gca().axvline(col, lw=1, color='k', zorder=1)
+
+        # Clear the content and mark.  Then show with zeros.
+        self.content = None
+        self.mark    = None
+        self.Show(np.zeros((self.rows, self.cols)))
+
+    def Flush(self):
+        # Show the plot.
+        plt.pause(0.001)
+
+    def Mark(self, row, col):
+        # Check the row/col arguments.
+        assert (row >= 0) and (row < self.rows), "Illegal row"
+        assert (col >= 0) and (col < self.cols), "Illegal col"
+
+        # Potentially remove the previous mark.
+        if self.mark is not None:
+            self.mark.remove()
+            self.mark = None
+
+        # Draw the mark.
+        self.mark  = plt.gca().text(0.5+col, 0.5+row, 'x', color = 'green',
+                                    verticalalignment='center',
+                                    horizontalalignment='center',
+                                    zorder=1)
+
+    def Grid(self, prob):
+        # Check the probability grid array size.
+        assert (np.size(prob, axis=0) == self.rows), "Inconsistent num of rows"
+        assert (np.size(prob, axis=1) == self.cols), "Inconsistent num of cols"
+
+        # Potentially remove the previous grid/content.
+        if self.content is not None:
+            self.content.remove()
+            self.content = None
+
+        # Create the color range.  There are clearly more elegant ways...
+        color = np.ones((self.rows, self.cols, 3))
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.walls[row,col]:
+                    color[row,col,0:3] = np.array([0.0, 0.0, 0.0])   # Black
+                else:
+                    # Shades of pink/purple/blue. Yellow means impossible.
+                    color[row,col,0:3] = np.array([1.0, 1.0, 1.0])
+                    # p    = prob[row,col]
+                    # pmin = 0.9 / self.spots
+                    # if p == 0:
+                    #     color[row,col,0:3] = np.array([1.0, 1.0, 0.0])
+                    # elif p < pmin:
+                    #     rlevel = (1.0 - p)
+                    #     glevel = (1.0 - p)
+                    #     color[row,col,0:3] = np.array([rlevel, glevel, 1.0])
+                    # else:
+                    #     rlevel = (1.0 - p)
+                    #     glevel = (pmin/p - pmin)
+                    #     color[row,col,0:3] = np.array([rlevel, glevel, 1.0])
+    
+        # Draw the boxes.
+        self.content = plt.gca().imshow(color,
+                                        aspect='equal',
+                                        interpolation='none',
+                                        extent=[0, self.cols, self.rows, 0],
+                                        zorder=0)
+
+    def Show(self, prob, pos = None):
+        # Update the content.
+        self.Grid(prob)
+
+        # Potentially add the mark.
+        if pos is not None:
+            self.Mark(pos[0], pos[1])
+
+        # Flush the figure.
+        self.Flush()
+
+
+#
+#  Robot (Emulate the actual robot)
+#
+#    probCmd is the probability that the command is actually executed
+#
+#    probProximal is a list of probabilities.  Each element
+#    corresponds to the probability that the proximity sensor will
+#    fire at a distance of (index+1).
+#
+class Robot():
+    def __init__(self, walls, row = 0, col = 0,
+                 probCmd = 1.0, probProximal = [1.0]):
+        # Check the row/col arguments.
+        assert (row >= 0) and (row < np.size(walls, axis=0)), "Illegal row"
+        assert (col >= 0) and (col < np.size(walls, axis=1)), "Illegal col"
+
+        # Report.
+        if walls[row, col]:
+            location = " (random location)"
+        else:
+            location = " (at %d, %d)" % (row, col)
+        print("Starting robot with real probCmd = " + str(probCmd) +
+              " and probProximal = " + str(probProximal) + location)
+
+        # Save the walls, the initial location, and the probabilities.
+        self.walls        = walls
+        self.row          = row
+        self.col          = col
+        self.probCmd      = probCmd
+        self.probProximal = probProximal
+
+        # Pick a valid starting location (if not already given).
+        while self.walls[self.row, self.col]:
+            self.row = random.randrange(0, np.size(walls, axis=0))
+            self.col = random.randrange(0, np.size(walls, axis=1))
+
+    def Command(self, drow, dcol):
+        # Check the delta.
+        assert ((abs(drow+dcol) == 1) and (abs(drow-dcol) == 1)), "Bad delta"
+        
+        # Try to move the robot the given delta.
+        row = self.row + drow
+        col = self.col + dcol
+        if (not self.walls[row, col]) and (random.random() < self.probCmd):
+            self.row = row
+            self.col = col
+
+    def Position(self):
+        return (self.row, self.col)
+
+    def Sensor(self, drow, dcol): 
+        # Check the delta.
+        assert ((abs(drow+dcol) == 1) and (abs(drow-dcol) == 1)), "Bad delta"
+
+        # Check the proximity in the given direction.
+        for k in range(len(self.probProximal)):
+            if self.walls[self.row + drow*(k+1), self.col + dcol*(k+1)]:
+                return (random.random() < self.probProximal[k])
+        return False
